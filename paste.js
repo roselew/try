713dee2306ead45f45b3b2bd2075d06011a14a6a
roselew <rozalewinska@gmail.com>
2021-12-01 23:54:03 +0100

    Paste 3

--- paste.js
+++ paste.js
@@ -1,34 +1,34 @@
 //How to read data
-(() => {
-    window.addEventListener(
-        "paste",
-        (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            window.pasteData = e.clipboardData.getData("application/json");
 
-            const data = window.pasteData;
-            const type = "text/json";
-            const filename = `${JSON.parse(window.pasteData).payload.nodes[0].data.attr.id}.json`;
+window.addEventListener(
+    "paste",
+    (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log("Wklejam");
+        window.pasteData = e.clipboardData.getData("application/json");
 
-            var file = new Blob([data], { type: type });
-            if (window.navigator.msSaveOrOpenBlob)
-                // IE10+
-                window.navigator.msSaveOrOpenBlob(file, filename);
-            else {
-                // Others
-                var a = document.createElement("a"),
-                    url = URL.createObjectURL(file);
-                a.href = url;
-                a.download = filename;
-                document.body.appendChild(a);
-                a.click();
-                setTimeout(function () {
-                    document.body.removeChild(a);
-                    window.URL.revokeObjectURL(url);
-                }, 0);
-            }
-        },
-        true
-    );
-})();
+        const data = window.pasteData;
+        const type = "text/json";
+        const filename = `${JSON.parse(window.pasteData).payload.nodes[0].data.attr.id}.json`;
+
+        var file = new Blob([data], { type: type });
+        if (window.navigator.msSaveOrOpenBlob)
+            // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else {
+            // Others
+            var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function () {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
+    },
+    true
+);
